10ab742bdea936615f72d2de62cc04a20fe5c744
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-06-14 09:20:19 +0000

    1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,9 +4,12 @@
 -- id, integer, never null, auto increment and primary key
 -- email, string (255 characters), never null and unique
 -- name, string (255 characters)
+-- country, enumeration of countries: US, CO and TN,
+--          never null (= default will be the first element of the enumeration, here US)
 
 Create Table IF NOT EXISTS users(
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255)
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 )
