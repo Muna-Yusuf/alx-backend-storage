99dd2f718cd1216d7c5b975fb34206c72ef29b61
Muna-Yusuf <muna.y.a.jim@gmail.com>
2024-06-19 12:16:04 +0000

    ...

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,3 +18,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
+            str, bytes, int, float, None]:
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key:str) -> Optional[str]:
+        """Fetch data from Redis using the key 
+           and convert it to a string."""
+        return self.get(key, lambda d: d.decode('uft-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieve data from Redis by key and convert it to an integer."""
+        return self.get(key, lambda d: int(d))
