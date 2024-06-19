d2a3df603557009dc03589c216e0ea4ae0da30cf
Muna-Yusuf <muna.y.a.jim@gmail.com>
2024-06-19 13:46:50 +0000

    Retrieving lists

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,44 +1,36 @@
 #!/usr/bin/env python3
-"""Web caching and tracking system."""
+"""
+web cache and tracker
+"""
 import requests
 import redis
 from functools import wraps
-import time
 
-redis_client = redis.Redis()
+store = redis.Redis()
 
-def count_calls(func):
-    """Decorator to count the number of times a function is called."""
-    @wraps(func)
-    def wrapper(url, *args, **kwargs):
-        key = f"count:{url}"
-        redis_client.incr(key)
-        return func(url, *args, **kwargs)
+
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
     return wrapper
 
-def cache_result(timeout=10):
-    """Decorator to cache function results in Redis with expiration."""
-    def decorator(func):
-        @wraps(func)
-        def wrapper(url, *args, **kwargs):
-            key = f"cache:{url}"
-            cached_result = redis_client.get(key)
-            if cached_result:
-                return cached_result.decode('utf-8')
-            result = func(url, *args, **kwargs)
-            redis_client.setex(key, timeout, result)
-            return result
-        return wrapper
-    return decorator
 
-@count_calls
-@cache_result()
+@count_url_access
 def get_page(url: str) -> str:
-    """Fetches the HTML content from a URL."""
-    response = requests.get(url)
-    return response.text
-
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/https://example.org"
-    html = get_page(url)
-    print(html)
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
